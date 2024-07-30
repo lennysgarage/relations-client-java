3458676f45ab7bec7570ad64c8ab6050a13899e9
Jonathan Marcantonio <jmarcant@redhat.com>
2024-07-30 17:51:41 -0400

    okay??
    Signed-off-by: Jonathan Marcantonio <jmarcant@redhat.com>

--- runBuf.sh
+++ runBuf.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 DOCKER=$(command -v podman || command -v docker)
 
-$DOCKER run -v "$(pwd):/workspace" --workdir /workspace bufbuild/buf generate buf.build/project-kessel/relations-api
+$DOCKER run -v "$(pwd):/workspace" --workdir /workspace bufbuild/buf generate buf.build/project-kessel/relations-api:67b1cd9b6c954e10b6da430958fd90a7
